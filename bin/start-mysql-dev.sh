37a017b54da0aa8dc41ff8a77e7a7a88ff66791d
James Stormes <jstormes@stormes.net>
2017-06-08 13:52:38 -0500

    Added OS X/Linux startup glue script.

---
+++ bin/start-mysql-dev.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# This script starts a MySql docker container, then starts a
+# bash shell in the container.
+#
+# When the user exits the bash shell it stops the container.
+#
+#
+Container_Name="mysql-dev"
+MYSQL_ROOT_PASSWORD="mypassword"
+MYSQL_DATABASE_NAME="employees"
+EXTERNAL_MYSQL_PORT="4000"
+
+docker -v >/dev/null 2>&1 ||
+{
+    echo >&2 "Docker not installed, the 'docker' command is required to run this scrip.";
+    exit 1;
+}
+
+project_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+
+docker run --detach --rm --name=$Container_Name -h $Container_Name -v "$project_root":/opt/project -w /opt/project \
+    -p ${EXTERNAL_MYSQL_PORT}:3306 \
+    -e="MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" -e="MYSQL_DATABASE_NAME=$MYSQL_DATABASE_NAME" \
+    -e="EXTERNAL_MYSQL_PORT=$EXTERNAL_MYSQL_PORT" mysql
+docker exec -it $Container_Name bash -c 'exec bash /opt/project/bin/bashrc'
+docker stop $Container_Name
