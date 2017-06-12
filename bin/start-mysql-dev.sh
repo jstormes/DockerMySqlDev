293f9e052f3236178f53452298c0c84d9f7d8e4e
James Stormes <james.s@digitalroominc.com>
2017-06-12 11:06:45 -0500

    Updated OS X/Linux bash scrip to match windows script.  UnTested.

--- bin/start-mysql-dev.sh
+++ bin/start-mysql-dev.sh
@@ -5,7 +5,7 @@
 # When the user exits the bash shell it stops the container.
 #
 #
-Container_Name="mysql-dev"
+CONTAINER_NAME="mysql-dev"
 MYSQL_ROOT_PASSWORD="mypassword"
 MYSQL_DATABASE_NAME="employees"
 EXTERNAL_MYSQL_PORT="4000"
@@ -19,9 +19,9 @@ docker -v >/dev/null 2>&1 ||
 
 project_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
-docker run --detach --rm --name=${Container_Name} -h ${Container_Name} -v "$project_root":/opt/project -w /opt/project \
+docker run --detach --rm --name=${CONTAINER_NAME} -h ${CONTAINER_NAME} -v "$project_root":/opt/project -w /opt/project \
     -p ${EXTERNAL_MYSQL_PORT}:3306 \
     -e="MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" -e="MYSQL_DATABASE_NAME=$MYSQL_DATABASE_NAME" \
-    -e="EXTERNAL_MYSQL_PORT=$EXTERNAL_MYSQL_PORT" ${DOCKER_HUB_IMAGE}
-docker exec -it ${Container_Name} bash -c 'exec bash /opt/project/bin/bashrc'
-docker stop ${Container_Name}
+    -e="EXTERNAL_MYSQL_PORT=$EXTERNAL_MYSQL_PORT" -e="CONTAINER_NAME=$CONTAINER_NAME" ${DOCKER_HUB_IMAGE}
+docker exec -it ${CONTAINER_NAME} bash -c 'exec bash /opt/project/bin/bashrc'
+docker stop ${CONTAINER_NAME}
